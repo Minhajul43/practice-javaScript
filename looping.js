73697129eacf5ea52344c650a55e31a0c9291f66
Minhajul43 <minhajul255685islam@gmail.com>
2026-01-31 11:59:47 +0600

    start-javaScript

---
+++ looping.js
@@ -0,0 +1,10 @@
+// looping problem
+// for loop
+//while loop
+// do while loop --> ignore
+
+const friends = ['Minhajul', 'Juel', 'Tahmeed', 'Neha'];
+
+for (const friend of friends) {
+  console.log(friend);
+}
