5f86002336664f59158608c5a44b263ba0ee0367
Minhajul43 <minhajul255685islam@gmail.com>
2026-01-31 12:16:36 +0600

    while loop

--- looping.js
+++ looping.js
@@ -2,9 +2,15 @@
 // for loop
 //while loop
 // do while loop --> ignore
+// for of ---> array loop korar jonno 
+// for in ---> object loop korar jonno
 
 const friends = ['Minhajul', 'Juel', 'Tahmeed', 'Neha'];
 
 for (const friend of friends) {
-  console.log(friend);
+  // console.log(friend);
+}
+
+for (let i = 0; i < friends.length; i++){
+  console.log(i+1, ":", friends[i]);
 }
---
+++ while-loop.js
@@ -0,0 +1,7 @@
+const friends = ['Minhajul', 'Juel', 'Tahmeed', 'Neha'];
+
+let i = 0;
+while (i < friends.length) {
+  console.log(i + 1, ".", friends[i]);
+  i++;
+}
